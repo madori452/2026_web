f8f38996edcf0135570c9047a000bd95f49a735d
Tracy Tung <d820430@yahoo.com.tw>
2026-06-03 14:53:24 +0800

    loading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './pages/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LoadingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  isLoading = true;
+
+  ngOnInit(): void {
+    // 顯示 loading page 2 秒，然後加載內容
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoadingComponent } from './pages/loading/loading.component';
 
 export const routes: Routes = [
+  { path: 'loading', component: LoadingComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '' },
 ];
