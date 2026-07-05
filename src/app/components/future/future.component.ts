1b863c73dc59450f82406dc353f7eb993f3b37ea
Tracy Tung <d820430@yahoo.com.tw>
2026-07-05 23:50:24 +0800

    字體調整

--- src/app/components/future/future.component.ts
+++ src/app/components/future/future.component.ts
@@ -20,18 +20,14 @@ export class FutureComponent {
         body: [
           '這些年來我從前端設計師出發，透過自學程式語言與經驗累積，逐步轉職為前端工程師。',
           '我知道仍有許多需要學習與精進的地方，也了解非本科背景需要投入更多時間與努力。',
-          '我期許自己能持續提升前端技術能力，並結合過去在UI設計與前端開發上的雙重經驗， ',
+          '我期許自己能持續提升前端技術能力，並結合過去在UI設計與前端開發上的雙重經驗。 ',
           '在工作中發揮所長，打造兼具功能性、使用體驗與視覺品質的產品。',
         ],
       },
       en: {
         title: 'Looking Ahead',
         body: [
-          'My journey began as a UI/Frontend Designer, and through self-learning and practical experience,',
-          'I gradually transitioned into a Frontend Engineer. Although I know there is still much to learn,',
-          'I believe my background in design gives me a unique perspective when building frontend products.',
-          'Moving forward, I hope to continue improving my technical skills while combining my design experience with frontend development.',
-          'I aim to create products that are not only functional,but also thoughtful,intuitive, and user-friendly.',
+          'My journey began as a UI/Frontend Designer, and through self-learning and practical experienceI gradually transitioned into a Frontend Engineer. Although I know there is still much to learn,I believe my background in design gives me a unique perspective when building frontend products.Moving forward, I hope to continue improving my technical skills while combining my design experience with frontend development.I aim to create products that are not only functional,but also thoughtful,intuitive, and user-friendly.',
         ],
       },
     };
